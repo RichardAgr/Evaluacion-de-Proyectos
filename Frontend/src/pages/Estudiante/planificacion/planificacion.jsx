99ceb899138650404ff9907fed641f2385965a6d
jHOkE <jhonumss@gmail.com>
2024-09-16 12:21:15 -0400

    comentarioSeparado

--- Frontend/src/pages/Estudiante/planificacion/planificacion.jsx
+++ Frontend/src/pages/Estudiante/planificacion/planificacion.jsx
@@ -9,11 +9,13 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
+import ComentarioNota from '../../../components/comentarioNota/comentarioNota.jsx';
 function planificacion() {
   function createData(hito, fechaIni, fechaFin, cobro, fechaEntrega, entregables) {
     return { hito, fechaIni, fechaFin, cobro, fechaEntrega, entregables };
   }
-  
+  const comentario = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis dolore doloribus iusto suscipit reiciendis autem libero quae, voluptates, iste dignissimos, nihil quos architecto. At sapiente deleniti, molestias assumenda omnis hic!';
+  const nota = 50;
   const rows = [
     createData('Sprint 1', 159, 6.0, 24, 4.0, 'Modelo ER'),
     createData('Sprint 2', 237, 9.0, 37, 4.3, 'Modelo ER'),
@@ -25,38 +27,42 @@ function planificacion() {
     <Fragment>
       <Header></Header>
         <div className='container'>
-          hola
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Hito</TableCell>
-                  <TableCell align="left">Fecha Inicio</TableCell>
-                  <TableCell align="left">Fecha Fin</TableCell>
-                  <TableCell align="left">Cobro</TableCell>
-                  <TableCell align="left">Fecha Entrega</TableCell>
-                  <TableCell align="left">Entregables</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <TableRow
-                    key={row.name}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      {row.hito}
-                    </TableCell>
-                    <TableCell align="left">{row.fechaIni}</TableCell>
-                    <TableCell align="left">{row.fechaFin}</TableCell>
-                    <TableCell align="left"><Button variant='contained'>Ver</Button></TableCell>
-                    <TableCell align="lefts">{row.fechaEntrega}</TableCell>
-                    <TableCell align="lefts">{row.entregables}</TableCell>
+          <Button variant='contained'>Atras</Button>
+          <h1>Planificacion</h1>
+          <div className='pageBorder'>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Hito</TableCell>
+                    <TableCell align="left">Fecha Inicio</TableCell>
+                    <TableCell align="left">Fecha Fin</TableCell>
+                    <TableCell align="left">Cobro</TableCell>
+                    <TableCell align="left">Fecha Entrega</TableCell>
+                    <TableCell align="left">Entregables</TableCell>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.hito}
+                      </TableCell>
+                      <TableCell align="left">{row.fechaIni}</TableCell>
+                      <TableCell align="left">{row.fechaFin}</TableCell>
+                      <TableCell align="left">{row.cobro}</TableCell>
+                      <TableCell align="lefts">{row.fechaEntrega}</TableCell>
+                      <TableCell align="lefts">{row.entregables}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <ComentarioNota comentario={comentario} nota = {nota}></ComentarioNota>
+          </div>
         </div>  
       <Footer></Footer>
     </Fragment>
