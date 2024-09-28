e50edf56f3dfc8e0cba9a81790592848490d1cc1
JhairCJ <jhaircjxd@gmail.com>
2024-09-28 11:36:47 -0400

    Eliminado de Lista de semanas y lista de tareas

--- Frontend/src/components/nav/nav.jsx
+++ Frontend/src/components/nav/nav.jsx
@@ -10,15 +10,16 @@ import ListaEmpresas from '../../pages/Docente/listaEmpresas/listaEmpresas.jsx';
 import HomeEstudiante from '../../pages/Estudiante/homeEstudiante/homeEstudiante.jsx'
 import HomeGrupoEstudiante from '../../pages/Estudiante/homeGrupoEstudiante/homeGrupoEstudiante.jsx'
 import VerPlanficacionDeDesarollo from '../../pages/Estudiante/verPlanificacionDeDesarollo/VerPlanifacionDeDesarolloE.jsx';
-import ListaSemanas from '../../pages/Estudiante/planificacion/hito/listaSemanas.jsx';
-import ListaTareas from '../../pages/Estudiante/planificacion/hito/semana/listaTareas.jsx';
+
 import EditarPlanificacion from '../../pages/Estudiante/editarPlanificacion/editarPlanificacion.jsx';
 import ValidarPlanificacion from '../../pages/Docente/validarPlanificacion/validarPlanificacion.jsx';
 
 function Nav() {
   
   return (
+    
     <Routes>
+        {/** Rutas para el docente */}
         {/** Ruta para sitios incorrectos */}
         <Route path="*" element={<Navigate to="/" />} />
         
@@ -32,17 +33,18 @@ function Nav() {
         <Route path='/homeDocente/homeGrupoDocente/verPlanificacionDeEmpresas/Empresa/:idEmpresa' element={<VerPlanificacionDeDesarolloD/>}/>
 
         {/** Rutas para validar  planificacion */}
-        <Route path='/grupoDocente/validarPlanificacion/' element={<ListaEmpresas/>}/>
+        <Route 
+          path='/grupoDocente/validarPlanificacion/' 
+          element={<ListaEmpresas/>}
+        />
+        
         <Route path='/grupoDocente/validarPlanificacion/Empresa/:idEmpresa' element={<ValidarPlanificacion/>}/>
 
         <Route path='/homeEstudiante' element={<HomeEstudiante/>}/>
         <Route path='/homeEstudiante/homeGrupoEstudiante' element={<HomeGrupoEstudiante/>}/>
         <Route path='/homeEstudiante/homeGrupoEstudiante/PlanificacionDeDesarollo/Empresa/:idEmpresa' element={<VerPlanficacionDeDesarollo/>}/>
         <Route path='/homeEstudiante/homeGrupoEstudiante/PlanificacionInicial/Empresa/:idEmpresa' element={<EditarPlanificacion/>}/>
-
-        {/** Rutas para ver la lista de semanas y tareas */}
-        <Route path='/grupoEmpresa/planificacion/:idEmpresa/sprint/semana' element={<ListaSemanas/>}/>
-        <Route path='/grupoEmpresa/planificacion/:idEmpresa/sprint/semana/tareas' element={<ListaTareas/>}/>
+        
     </Routes>
   )
 }
