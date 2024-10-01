8675a5a20e5dab0c37440e40a9ae188a2305d358
Eth4nSP <eth4n.gerl@gmail.com>
2024-09-30 22:04:36 -0400

    PRUEBA DE FOR

--- Backend/app/Http/Controllers/TareaController.php
+++ Backend/app/Http/Controllers/TareaController.php
@@ -126,6 +126,9 @@ class TareaController extends Controller
 
         // Convierte el resultado de archivos a un array
         $archivosArray = $archivosTarea->pluck('archivo','nombreArchivo')->toArray();*/
+
+        //================= USANDO HASHMAP =============================
+        /*
         $archivosTarea = DB::table('archivostarea')
         ->join('tarea', function($join) use ($idTarea) {
                 $join->on('archivostarea.idTarea', '=', 'tarea.idTarea')
@@ -142,6 +145,25 @@ class TareaController extends Controller
                     'archivo' => $item->archivo,
                 ];
         })->toArray();
+        */
+
+        //===================================== UTILIZANDO FOREACH ==========================
+        $archivosTarea = DB::table('archivostarea')
+        ->join('tarea', function($join) use ($idTarea) {
+            $join->on('archivostarea.idTarea', '=', 'tarea.idTarea')
+                ->whereRaw('DATE(archivostarea.fechaEntrega) = DATE(tarea.fechaEntrega)');
+        })
+        ->where('tarea.idTarea', $idTarea)
+        ->select('archivostarea.archivo', 'archivostarea.nombreArchivo')
+        ->get();
+
+    // Convierte el resultado de archivos a un array en el formato deseado
+    $archivosArray = []; // Inicializa el array
+
+    foreach ($archivosTarea as $item) {
+        $archivosArray[] = [$item->nombreArchivo, $item->archivo]; // Añade un array con el nombre y el archivo
+    }
+
 
 
         // Formar la respuesta
