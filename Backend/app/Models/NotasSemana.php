04814912975c77bd3549676240c4447ff9048a77
Eth4nSP <eth4n.gerl@gmail.com>
2024-10-21 14:58:49 -0400

    error

--- Backend/app/Models/NotasSemana.php
+++ Backend/app/Models/NotasSemana.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -39,30 +38,5 @@ class NotasSemana extends Model
     public function estudiante()
     {
         return $this->belongsTo(Estudiante::class, 'idEstudiante', 'idEstudiante');
-=======
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-
-class NotasSemana extends Model
-{
-    protected $table = 'notassemana';
-    protected $primaryKey = 'idNota';
-    public $incrementing = false;
-    public $timestamps = false;
-    protected $fillable = [
-        'idSemana',
-        'idEstudiante',
-        'nota'
-    ];
-
-    public function estudiante()
-    {
-        return $this->belongsTo(Estudiante::class, 'idEstudiante');
-    }
-
-    public function semana()
-    {
-        return $this->belongsTo(Semana::class, 'idSemana');
->>>>>>> origin/adrian
     }
 }
