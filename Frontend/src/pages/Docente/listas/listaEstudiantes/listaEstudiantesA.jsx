9d0b85af4b88b2629586580de163b673a7af17cb
joacos4545 <villarpando.joaquin@gmail.com>
2024-10-24 13:25:58 -0400

    merge con rama jhair

--- Frontend/src/pages/Docente/listas/listaEstudiantes/listaEstudiantesA.jsx
+++ Frontend/src/pages/Docente/listas/listaEstudiantes/listaEstudiantesA.jsx
@@ -30,125 +30,35 @@ const columns = [
 ];
 
 const rows = [
-  { id: 1, nombreEstudiante: 'Jon', apellidoPaternoEstudiante: 'Snow', apellidoMaternoEstudiante: 'Stark', equipo: 'prueba1', grupo: 35 },
-  { id: 2, nombreEstudiante: 'Cersei', apellidoPaternoEstudiante: 'Lannister', apellidoMaternoEstudiante: 'Baratheon', equipo: 'prueba1', grupo: 42 },
-  { id: 3, nombreEstudiante: 'Jaime', apellidoPaternoEstudiante: 'Lannister', apellidoMaternoEstudiante: 'Baratheon', equipo: 'prueba1', grupo: 45 },
-  { id: 4, nombreEstudiante: 'Arya', apellidoPaternoEstudiante: 'Stark', apellidoMaternoEstudiante: 'Stark', equipo: 'prueba1', grupo: 16 }, // Asegúrate de que sea 'grupo' en lugar de 'aggrupoe'
-  { id: 5, nombreEstudiante: 'Daenerys', apellidoPaternoEstudiante: 'Targaryen', apellidoMaternoEstudiante: 'Targaryen', equipo: 'prueba1', grupo: null },
-  { id: 6, nombreEstudiante: 'Tyrion', apellidoPaternoEstudiante: 'Lannister', apellidoMaternoEstudiante: 'Tytos', equipo: 'prueba2', grupo: 38 },
-  { id: 7, nombreEstudiante: 'Bran', apellidoPaternoEstudiante: 'Stark', apellidoMaternoEstudiante: 'Stark', equipo: 'prueba2', grupo: 12 },
-  { id: 8, nombreEstudiante: 'Sansa', apellidoPaternoEstudiante: 'Stark', apellidoMaternoEstudiante: 'Tully', equipo: 'prueba2', grupo: 22 },
-  { id: 9, nombreEstudiante: 'Brienne', apellidoPaternoEstudiante: 'Tarth', apellidoMaternoEstudiante: 'Unknown', equipo: 'prueba3', grupo: 32 },
-  { id: 10, nombreEstudiante: 'Samwell', apellidoPaternoEstudiante: 'Tarly', apellidoMaternoEstudiante: 'Farlund', equipo: 'prueba3', grupo: 27 },
-  { id: 11, nombreEstudiante: 'Gendry', apellidoPaternoEstudiante: 'Waters', apellidoMaternoEstudiante: 'Unknown', equipo: 'prueba3', grupo: 28 },
-  { id: 12, nombreEstudiante: 'Theon', apellidoPaternoEstudiante: 'Greyjoy', apellidoMaternoEstudiante: 'Harlaw', equipo: 'prueba4', grupo: 30 },
-  { id: 13, nombreEstudiante: 'Yara', apellidoPaternoEstudiante: 'Greyjoy', apellidoMaternoEstudiante: 'Harlaw', equipo: 'prueba4', grupo: 35 },
-  { id: 14, nombreEstudiante: 'Jorah', apellidoPaternoEstudiante: 'Mormont', apellidoMaternoEstudiante: 'Unknown', equipo: 'prueba4', grupo: 48 },
-  { id: 15, nombreEstudiante: 'Daario', apellidoPaternoEstudiante: 'Naharis', apellidoMaternoEstudiante: 'Unknown', equipo: 'prueba5', grupo: 33 },
-  { id: 16, nombreEstudiante: 'Missandei', apellidoPaternoEstudiante: 'Unknown', apellidoMaternoEstudiante: 'Unknown', equipo: 'prueba5', grupo: 24 },
-  { id: 17, nombreEstudiante: 'Eddard', apellidoPaternoEstudiante: 'Stark', apellidoMaternoEstudiante: 'Tully', equipo: 'prueba5', grupo: 50 },
-  { id: 18, nombreEstudiante: 'Robb', apellidoPaternoEstudiante: 'Stark', apellidoMaternoEstudiante: 'Tully', equipo: 'prueba5', grupo: 24 },
-  { id: 19, nombreEstudiante: 'Oberyn', apellidoPaternoEstudiante: 'Martell', apellidoMaternoEstudiante: 'Unknown', equipo: 'prueba6', grupo: 45 },
-  { id: 20, nombreEstudiante: 'Ellaria', apellidoPaternoEstudiante: 'Sand', apellidoMaternoEstudiante: 'Unknown', equipo: 'prueba6', grupo: 39 },
+  { id: 1, nombreEstudiante: 'Jon', apellidoPaternoEstudiante: 'Snow', apellidoMaternoEstudiante: 'Stark', nombreEmpresa: 'prueba1', grupo: 35 },
+  { id: 2, nombreEstudiante: 'Cersei', apellidoPaternoEstudiante: 'Lannister', apellidoMaternoEstudiante: 'Baratheon', nombreEmpresa: 'prueba1', grupo: 42 },
+  { id: 3, nombreEstudiante: 'Jaime', apellidoPaternoEstudiante: 'Lannister', apellidoMaternoEstudiante: 'Baratheon', nombreEmpresa: 'prueba1', grupo: 45 },
+  { id: 4, nombreEstudiante: 'Arya', apellidoPaternoEstudiante: 'Stark', apellidoMaternoEstudiante: 'Stark', nombreEmpresa: 'prueba1', grupo: 16 }, // Asegúrate de que sea 'grupo' en lugar de 'aggrupoe'
+  { id: 5, nombreEstudiante: 'Daenerys', apellidoPaternoEstudiante: 'Targaryen', apellidoMaternoEstudiante: 'Targaryen', nombreEmpresa: 'prueba1', grupo: null },
+  { id: 6, nombreEstudiante: 'Tyrion', apellidoPaternoEstudiante: 'Lannister', apellidoMaternoEstudiante: 'Tytos', nombreEmpresa: 'prueba2', grupo: 38 },
+  { id: 7, nombreEstudiante: 'Bran', apellidoPaternoEstudiante: 'Stark', apellidoMaternoEstudiante: 'Stark', nombreEmpresa: 'prueba2', grupo: 12 },
+  { id: 8, nombreEstudiante: 'Sansa', apellidoPaternoEstudiante: 'Stark', apellidoMaternoEstudiante: 'Tully', nombreEmpresa: 'prueba2', grupo: 22 },
+  { id: 9, nombreEstudiante: 'Brienne', apellidoPaternoEstudiante: 'Tarth', apellidoMaternoEstudiante: 'Unknown', nombreEmpresa: 'prueba3', grupo: 32 },
+  { id: 10, nombreEstudiante: 'Samwell', apellidoPaternoEstudiante: 'Tarly', apellidoMaternoEstudiante: 'Farlund', nombreEmpresa: 'prueba3', grupo: 27 },
+  { id: 11, nombreEstudiante: 'Gendry', apellidoPaternoEstudiante: 'Waters', apellidoMaternoEstudiante: 'Unknown', nombreEmpresa: 'prueba3', grupo: 28 },
+  { id: 12, nombreEstudiante: 'Theon', apellidoPaternoEstudiante: 'Greyjoy', apellidoMaternoEstudiante: 'Harlaw', nombreEmpresa: 'prueba4', grupo: 30 },
+  { id: 13, nombreEstudiante: 'Yara', apellidoPaternoEstudiante: 'Greyjoy', apellidoMaternoEstudiante: 'Harlaw', nombreEmpresa: 'prueba4', grupo: 35 },
+  { id: 14, nombreEstudiante: 'Jorah', apellidoPaternoEstudiante: 'Mormont', apellidoMaternoEstudiante: 'Unknown', nombreEmpresa: 'prueba4', grupo: 48 },
+  { id: 15, nombreEstudiante: 'Daario', apellidoPaternoEstudiante: 'Naharis', apellidoMaternoEstudiante: 'Unknown', nombreEmpresa: 'prueba5', grupo: 33 },
+  { id: 16, nombreEstudiante: 'Missandei', apellidoPaternoEstudiante: 'Unknown', apellidoMaternoEstudiante: 'Unknown', nombreEmpresa: 'prueba5', grupo: 24 },
+  { id: 17, nombreEstudiante: 'Eddard', apellidoPaternoEstudiante: 'Stark', apellidoMaternoEstudiante: 'Tully', nombreEmpresa: 'prueba5', grupo: 50 },
+  { id: 18, nombreEstudiante: 'Robb', apellidoPaternoEstudiante: 'Stark', apellidoMaternoEstudiante: 'Tully', nombreEmpresa: 'prueba5', grupo: 24 },
+  { id: 19, nombreEstudiante: 'Oberyn', apellidoPaternoEstudiante: 'Martell', apellidoMaternoEstudiante: 'Unknown', nombreEmpresa: 'prueba6', grupo: 45 },
+  { id: 20, nombreEstudiante: 'Ellaria', apellidoPaternoEstudiante: 'Sand', apellidoMaternoEstudiante: 'Unknown', nombreEmpresa: 'prueba6', grupo: 39 },
 ];
 
 const paginationModel = { page: 0, pageSize: 20 };
 
 export default function DataTable() {
-  const [filteredRows, setFilteredRows] = useState(rows); 
-  const [searchValue, setSearchValue] = useState(''); 
-  const [estudiantes, setEstudiantes] = useState([]);
-  const [loading, setLoading] = useState(true); 
-  const [error, setError] = useState(null);
-  const [hasFetchedData, setHasFetchedData] = useState(false);
   const idGrupo = 1; // Hardcodeado
   const gestionGrupo = '2024-2'; // Hardcodeado
 
 
-
-  
-  const fetchEstudiantes = async () => {
-    try {
-      const response = await fetch(`http://localhost:8000/api/grupo/estudiantes/${idGrupo}/${gestionGrupo}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (!response.ok) {
-        throw new Error('Error de grupo');
-      }
-
-      const data = await response.json();
-      if (!hasFetchedData) {
-        setEstudiantes(data);
-        setFilteredRows(data);
-        setHasFetchedData(true); // Indica que los datos han sido cargados
-      }
-    } catch (error) {
-      console.error('Error en la solicitud:', error);
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
-  // Función para manejar la búsqueda en tiempo real
-  useEffect(() => {
-    const handleSearch = async () => {
-      if (!searchTerm.trim()) {
-        fetchEstudiantes(); // Volver a cargar todos los estudiantes si el término está vacío
-        return;
-      }
-
-      try {
-        const response = await fetch(`http://localhost:8000/api/grupo/estudiante/barraBusqueda`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ 
-            idGrupo, 
-            gestionGrupo, 
-            termino: searchTerm 
-          }),
-        });
-
-        if (!response.ok) {
-          throw new Error('Error en la búsqueda');
-        }
-
-        const result = await response.json();
-        // Ordenar alfabéticamente por el nombre
-        result.sort((a, b) => {
-          const nombreA = `${a.nombreEstudiante} ${a.apellidoPaternoEstudiante} ${a.apellidoMaternoEstudiante}`.toLowerCase();
-          const nombreB = `${b.nombreEstudiante} ${b.apellidoPaternoEstudiante} ${a.apellidoMaternoEstudiante}`.toLowerCase();
-          return nombreA.localeCompare(nombreB);
-        });
-
-        setEstudiantes(result);
-      } catch (error) {
-        console.error('Error en la solicitud:', error);
-        setError(error.message);
-      }
-    };
-
-    handleSearch(); // Llamar a la búsqueda inmediatamente
-  }, [searchTerm]); // Solo se ejecuta cuando searchTerm cambia
-
-  const totalPaginas = Math.ceil(estudiantes.length / estudiantesPorPagina);
-  const indexOfLastEstudiante = currentPage * estudiantesPorPagina;
-  const indexOfFirstEstudiante = indexOfLastEstudiante - estudiantesPorPagina;
-  const estudiantesActuales = estudiantes.slice(indexOfFirstEstudiante, indexOfLastEstudiante);
-
-  const handleNextPage = () => {
-    if (currentPage < totalPaginas) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  if (loading) return <div>Cargando...</div>; // Manejo de carga
-  if (error) return <div>Error: {error}</div>; // Manejo de errores
 
 
   return (
@@ -160,15 +70,14 @@ export default function DataTable() {
           disableListWrap
           options={[]}
           sx={{ width: 300, marginBottom: '16px' }}
-          onInputChange={handleSearchChange}
           renderInput={(params) => <TextField {...params} label="Buscar estudiante, equipo o grupo" />}
         />
 
         <Paper sx={{ height: 700, width: '100%' }}>
           <DataGrid
-            rows={filteredRows} 
+            rows={rows} 
             columns={columns}
-            getRowId={(row) => row.idEstudiante} 
+            // getRowId={(row) => row.idEstudiante} 
             initialState={{ pagination: { paginationModel } }}
             pageSizeOptions={[2, 10]}
             disableColumnMenu
