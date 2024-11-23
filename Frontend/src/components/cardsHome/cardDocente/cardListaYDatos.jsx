0543f02af25cb34d929f14aede0b567dc9f89724
jHOkE <jhonumss@gmail.com>
2024-11-23 13:14:26 -0400

    cambiosMinimos

--- Frontend/src/components/cardsHome/cardDocente/cardListaYDatos.jsx
+++ Frontend/src/components/cardsHome/cardDocente/cardListaYDatos.jsx
@@ -9,9 +9,6 @@ function CardResumen() {
         info = {<>
         </>}
         buttons={<>
-        <Button variant="contained" color="primary" fullWidth onClick={() => navigate("/homeGrupo/1/listaEstudiantes/2024-2")} >
-            LISTA DE ESTUDIANTES
-        </Button>
         <Button variant="outlined" color="primary" fullWidth onClick={() => navigate("/homeGrupo/1/empresa/calificaciones")}>
             18_RECUPERAR CALIFICACIONES SPRINTS PREVIOS
         </Button>
