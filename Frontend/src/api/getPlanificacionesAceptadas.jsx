e85e830aa564a653559bec75dc0013b75035cd90
joacos4545 <villarpando.joaquin@gmail.com>
2024-12-03 10:24:00 -0400

    jhoke

--- Frontend/src/api/getPlanificacionesAceptadas.jsx
+++ Frontend/src/api/getPlanificacionesAceptadas.jsx
@@ -2,7 +2,7 @@
 export const getPlanificacionesAceptadas   = async () => {
   
     try {
-      const response = await fetch(`http://127.0.0.1:8000/api/planificacionAceptadas`, {
+      const response = await fetch(`http://localhost:8000/api/planificacionAceptadas`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
